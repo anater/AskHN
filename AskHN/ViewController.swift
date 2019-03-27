4d3de65c080e9480d866642b470f9a0b57b5f037
Andrew Nater <a.a.nater@gmail.com>
2019-03-27 13:58:32 -0400

    Set up basic UI for main screen

--- AskHN/ViewController.swift
+++ AskHN/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let data = ["one", "two", "three"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "story", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
 }
 
