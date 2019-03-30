e91eaba4b014fbba53f8a5fa2c46874ad4d1d902
Andrew Nater <a.a.nater@gmail.com>
2019-03-30 15:10:41 -0400

    Update API to properly decode JSON

--- AskHN/API.swift
+++ AskHN/API.swift
@@ -12,27 +12,21 @@ class HackerNewsAPI {
     
     let rootURL = "https://hacker-news.firebaseio.com/v0/"
     
-    private func request(endpoint: String, completionHandler: @escaping (Any?, Error?) -> Void) {
+    private func request(endpoint: String, completionHandler: @escaping (Data?, Error?) throws -> Void) {
         URLSession.shared.dataTask(with: url(for: endpoint)) { data, response, error in
-            print("request for \(endpoint)")
-            if let error = error {
-                completionHandler(nil, error)
-                return
-            }
-            
-            if let data = data,
-                let httpResponse = response as? HTTPURLResponse,
-                httpResponse.mimeType != nil && httpResponse.mimeType == "application/json" {
-                print("we have data!")
-                do {
-                    let options = JSONSerialization.ReadingOptions(arrayLiteral: .mutableContainers)
-                    let json = try JSONSerialization.jsonObject(with: data, options: options)
-                    completionHandler(json, nil)
-                } catch {
-                    print("json failed")
-                    let error = NSError(domain: "JSON Error", code: 0)
-                    completionHandler(nil, error)
+            do {
+                if let error = error {
+                    try completionHandler(nil, error)
                 }
+                
+                if let data = data,
+                    let httpResponse = response as? HTTPURLResponse,
+                    httpResponse.mimeType == "application/json" {
+                    try completionHandler(data, nil)
+                }
+            } catch let completionError {
+                print("Completion Handler Error")
+                print(completionError.localizedDescription)
             }
         }.resume()
     }
@@ -41,29 +35,53 @@ class HackerNewsAPI {
         return URL(string: rootURL + endpoint + ".json")!
     }
     
-    func ask(completionHandler: @escaping (Any?, Error?) -> Void) {
-        request(endpoint: "askstories", completionHandler: completionHandler)
+    func getAskStories(completionHandler: @escaping ([Int]?, Error?) -> Void) {
+        request(endpoint: "askstories") { (data, error) throws in
+            guard let data = data else { return }
+            guard error == nil else { return completionHandler(nil, error) }
+            do {
+                // attempt to decode response as Int array
+                let decoder = JSONDecoder()
+                let items = try decoder.decode([Int].self, from: data)
+                completionHandler(items, nil)
+            } catch let jsonError as DecodingError {
+                // if something went wrong with decoding, return the error
+                print(jsonError)
+                completionHandler(nil, jsonError)
+            }
+        }
     }
     
-    func item(withID id: Int, completionHandler: @escaping (Any?, Error?) -> Void) {
-        request(endpoint: "item/\(id)", completionHandler: completionHandler)
+    func getItem(with id: Int, completionHandler: @escaping (HNItem?, Error?) -> Void) {
+        request(endpoint: "item/\(id)") { (data, error) throws in
+            guard let data = data else { return }
+            guard error == nil else { return completionHandler(nil, error) }
+            do {
+                let decoder = JSONDecoder()
+                let item = try decoder.decode(HNItem.self, from: data)
+                completionHandler(item, nil)
+            } catch let jsonError as DecodingError {
+                print(jsonError)
+                completionHandler(nil, jsonError)
+            }
+        }
     }
     
-    func items(ids: Array<Int>, completionHandler: @escaping (Any?, Error?) -> Void) {
+    func getItems(for ids: [Int], completionHandler: @escaping ([HNItem]?, Error?) -> Void) {
         let group = DispatchGroup()
-        var responses = [Any]()
+        var responses = [HNItem]()
         
         for id in ids {
             group.enter()
-            item(withID: id) { (data, error) in
+            getItem(with: id) { (item, error) in
                 if error != nil {
                     let message = "⚠️ Error with item \(id)"
                     let error = NSError(domain: message, code: 2)
                     print(message)
                     completionHandler(nil, error)
                 }
-                if data != nil {
-                    responses.append(data!)
+                if let item = item {
+                    responses.append(item)
                 }
                 group.leave()
             }
@@ -82,9 +100,9 @@ struct HNItem: Codable {
     var by: String
     var descendants: Int
     var id: Int
-    var kids: Set<Int>?
+    var kids: [Int]?
     var score: Int
-    var text: String // TODO: use attributeable string to convert from HTML
+    var text: String? // TODO: use attributeable string to convert from HTML
     var time: Double // TODO: convert to date object from UNIX time
     var title: String // TODO: trim off Ask HN prefixes
     var type: ItemType
